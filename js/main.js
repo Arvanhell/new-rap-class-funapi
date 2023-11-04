7a62be9206339664c20a3b416ba8820e5fdff4a8
Arvanhell <arrivald@gmail.com>
2023-11-04 18:51:08 +0000

    Fixed cors errors

--- js/main.js
+++ js/main.js
@@ -1,2 +1,14 @@
-alert('working')
-console.log('Hallo World!');
+document.querySelector('button').addEventListener('click', apiRequest)
+
+async function apiRequest(){
+    const rapperName = document.querySelector('input').value
+    try{
+        const response = await fetch(`https://new-rap-class-funapi.vercel.app/api/${rapperName}`)
+        const data = await response.json()
+
+        console.log(data)
+        document.querySelector('h2').innerText = data.birthName
+    }catch(error){
+        console.log(error)
+    }
+}
